b070840439a2e16d0101ac58d5da837a6aeb6907
iower <iower@tutanota.com>
2021-03-06 22:52:57 +0500

    Add tree

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <Tree :value="treeData"></Tree>
   </div>
 </template>
+
+<script lang="ts">
+import 'he-tree-vue/dist/he-tree-vue.css'
+import { Tree, Draggable } from 'he-tree-vue'
+
+export default {
+  components: { Tree: Tree.mixPlugins([Draggable]) },
+  data() {
+    return {
+      treeData: [
+        {
+          text: 'node 1',
+        },
+        {
+          text: 'node 2',
+          children: [
+            {
+              text: 'node 2-1',
+            }
+          ]
+        }
+      ]
+    }
+  },
+}
+</script>
